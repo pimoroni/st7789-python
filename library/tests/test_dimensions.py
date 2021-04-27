c01f2fdad7e41f5a21359310a4241e08a0a840ea
Phil Howard <phil@gadgetoid.com>
2021-04-27 14:42:51 +0100

    Udpate tests

--- library/tests/test_dimensions.py
+++ library/tests/test_dimensions.py
@@ -1,16 +1,4 @@
-import sys
-import mock
-
-
-def _mock():
-    sys.modules['numpy'] = mock.Mock()
-    sys.modules['spidev'] = mock.Mock()
-    sys.modules['RPi'] = mock.Mock()
-    sys.modules['RPi.GPIO'] = mock.Mock()
-
-
-def test_240_240():
-    _mock()
+def test_240_240(GPIO, spidev, numpy):
     import ST7789
     display = ST7789.ST7789(port=0, cs=0, dc=24, width=240, height=240, rotation=0)
     assert display.width == 240
